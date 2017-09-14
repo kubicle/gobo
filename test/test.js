d5fa722cb75374e80ba0c1b19391ba3cc224c069
kubicle <kubicle@yahoo.com>
2017-09-14 21:43:01 +0900

    Version 0.2.2

--- test/test.js
+++ test/test.js
@@ -592,13 +592,15 @@ var cheapSeed = require('./cheapSeed');
 var random = cheapSeed.pseudoRandom;
 var randomBetween = cheapSeed.pseudoRandomBetween;
 var RED = 0, GREEN = 1, BLUE = 2;
-var AVERAGE_LINE_WIDTH_PX = 1.2; // decides how many vertical lines we have
-var LINE_DELTADELTA = 0.1;
-var MAX_DELTA = 0.25; // +/- max variation each line can move per pixel
-var GREEN_RED_RATIO = [0.47, 0.8]; // 0 allows red; 1 allows yellow
-var BLUE_GREEN_RATIO = [0.5, 0.9]; // 0 allows yellow; 1 allows pink
+var MAX_DELTA = 0.12; // +/- max variation each line can move per vertical pixel
+var LINE_DELTADELTA = MAX_DELTA / 8;
+var WARPZONE_X = 30, WARPZONE_Y = 100;
+var GREEN_RED_RATIO = [0.4, 0.85]; // 0 allows red; 1 allows yellow
+var BLUE_GREEN_RATIO = [0.6, 0.9]; // 0 allows yellow; 1 allows pink
 var DARKER_RATIO = [0.75, 0.9]; // 0 makes black; 1 gives same color
 var BRIGHT_DARK_REGULARITY = [0.2, 0.5]; // 0 makes total alternance bright/dark; 1 makes complete random
+var averageLineWidth; // decides how many vertical lines we have
+var maxDeviation; // how much a line can deviate "freely"
 function randowBrown() {
     var color = [];
     color[RED] = 1;
@@ -608,14 +610,14 @@ function randowBrown() {
 }
 function darkerColor(base) {
     var color = [0, 0, 0];
+    var ratio = randomBetween(DARKER_RATIO[0], DARKER_RATIO[1]);
     for (var c = 0; c < 3; c++) {
-        var ratio = randomBetween(DARKER_RATIO[0], DARKER_RATIO[1]);
         color[c] = base[c] * ratio;
     }
     return color;
 }
 function buildLineWidths(width, lineDeltas) {
-    var lineCount = ~~(width / AVERAGE_LINE_WIDTH_PX);
+    var lineCount = ~~(width / averageLineWidth);
     var remainingWidth = width;
     var lineWidths = [];
     for (var i = lineCount; i >= 2; i--) {
@@ -630,13 +632,21 @@ function buildLineWidths(width, lineDeltas) {
 function makeLinesVary(initLineWidths, lineWidths, lineDeltas, width, height, y) {
     var remainingWidth = width;
     var lineCount = lineWidths.length;
-    var yRatio = Math.min(y, height - y) / height;
+    var yWrap = Math.max((y - height) / WARPZONE_Y + 1, 0);
     for (var i = 0; i < lineCount - 1; i++) {
+        var perfectX = (i + 0.5) * averageLineWidth;
+        var x = (width - remainingWidth) + lineWidths[i] / 2;
+        var xWrap = Math.max((x - width) / WARPZONE_X + 1, 0);
+        var deviationRatio = Math.min(Math.abs(x - perfectX) / maxDeviation, 1);
+        var lineDelta = lineDeltas[i];
         var deltaDelta = (random() - 0.5) * LINE_DELTADELTA;
-        lineDeltas[i] = Math.max(Math.min(lineDeltas[i] + deltaDelta, MAX_DELTA), -MAX_DELTA);
-        var newWidth = (remainingWidth / (lineCount - i) + (lineWidths[i] + lineDeltas[i])) / 2;
-        lineWidths[i] = initLineWidths[i] * (1 - yRatio) + newWidth * yRatio;
-        remainingWidth -= lineWidths[i];
+        lineDelta = lineDelta * (1 - deviationRatio) + deltaDelta;
+        lineDeltas[i] = lineDelta = Math.max(Math.min(lineDelta, MAX_DELTA), -MAX_DELTA);
+        var w = lineWidths[i] + lineDelta;
+        w = remainingWidth / (lineCount - i) * xWrap + w * (1 - xWrap);
+        var newWidth = initLineWidths[i] * yWrap + w * (1 - yWrap);
+        lineWidths[i] = newWidth;
+        remainingWidth -= newWidth;
     }
     lineWidths[lineCount - 1] = remainingWidth;
 }
@@ -657,6 +667,8 @@ function buildLineColors(lineCount) {
     return lineColors;
 }
 function drawWood(canvas) {
+    averageLineWidth = randomBetween(1.2, 2.4);
+    maxDeviation = randomBetween(1, 2.5) * averageLineWidth;
     var lineDeltas = [];
     var initLineWidths = buildLineWidths(canvas.width, lineDeltas);
     var lineCount = initLineWidths.length;
